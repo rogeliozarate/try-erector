a4958228305320bf6bd350a5380369f9d93012f5
Rogelio Zarate <i.am@rogeliozarate.com>
2013-08-22 19:41:20 -0500

    Create minimal widget with erector
    - It is very interesting to create HTML as coded ruby

---
+++ app.rb
@@ -0,0 +1,25 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'erector'
+
+
+class Hello < Erector::Widget
+  def content
+    html {
+      head {
+        title "Hello"
+      }
+      body {
+        h1.heading! "Message:"
+        text "Hello, "
+        b.big @target
+        text "!"
+      }
+    }
+  end
+end
+
+
+get '/' do
+  Hello.new(:target => 'world').to_html
+end
